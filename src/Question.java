7efd3e1cba8ccfaac704c133480f18f2d1fd13d9
PattyBei <tp6qo41o428@gmail.com>
2015-12-21 23:42:50 +0800

    ver4

--- src/Question.java
+++ src/Question.java
@@ -1,22 +1,26 @@
 import java.util.ArrayList;
-//12311231
+
 //D0208613 陳佩貝
 class Question {
    String description;
+   
    public Question(String desc) {
       this.description = desc;
    }
+   
    public void print() {
-      System.out.println(desc);
+      System.out.println(this.description);
    }      
-
 }
-
+//----------------------------------
 abstract class ExamPaper {
+	
     public ExamPaper() {
     }
+    
     abstract void op () ;
 }
+
 class Topic extends ExamPaper {
 
     public Topic() {
@@ -25,13 +29,22 @@ class Topic extends ExamPaper {
 	@Override
 	void op() {		
 	}
-
 }
+//----------------------------------
 class OtherPaperTopic extends ExamPaper {
 	ArrayList <ExamPaper > gList ;
+	String description;
 	
     public OtherPaperTopic() {
     }
+    
+    public OtherPaperTopic(String desc) {
+       this.description = desc;
+    }
+    
+    public void print() {
+       System.out.println(this.description);
+    }  
     
 	@Override
 	void op() {	
