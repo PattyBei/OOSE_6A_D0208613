bb539625806129de8601227a362c0114bd0d497b
PattyBei <tp6qo41o428@gmail.com>
2015-12-21 23:37:50 +0800

    ExamPaper Topic OtherPaperTopic

--- src/Question.java
+++ src/Question.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+//12311231
 //D0208613 陳佩貝
 class Question {
    String description;
@@ -9,9 +11,29 @@ class Question {
    }      
 
 }
-interface exam{
-	   
+
+abstract class ExamPaper {
+    public ExamPaper() {
+    }
+    abstract void op () ;
 }
-class Composite {
+class Topic extends ExamPaper {
+
+    public Topic() {
+    }
+
+	@Override
+	void op() {		
+	}
+
+}
+class OtherPaperTopic extends ExamPaper {
+	ArrayList <ExamPaper > gList ;
 	
+    public OtherPaperTopic() {
+    }
+    
+	@Override
+	void op() {	
+	}
 }
